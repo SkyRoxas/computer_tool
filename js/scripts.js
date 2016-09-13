9b44f7b4cdb6e619154522c8858bfffdcd921bd4
SkyRoxas <SkyRoxas1521@gmail.com>
2016-09-13 17:34:35 +0800

    button input

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
+function numOput($num) {
+    if (document.getElementById('result').value == 0) {
+        document.getElementById('result').value = $num;
+    } else {
+        document.getElementById('result').value += $num;
+    }
+}
 
+function numReSet() {
+    document.getElementById('result').value = 0;
+}
+
+function temp() {
+    var temp = document.getElementById('result').value;
+    document.getElementById('result').value = 0;
+
+    console.log("temp:" + temp);
+
+    return temp;
+
+}
